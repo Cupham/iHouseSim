0970f9ed70555342d017d9781cfb2c471a185cda
Cu Pham <cupham@jaist.ac.jp>
2019-06-17 23:13:58 +0900

    create iHouseSime
    Signed-off-by: Cu Pham <cupham@jaist.ac.jp>

---
+++ iHouseSim/Assets/IDevice.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IDevice : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TurnOnMe()
+    {
+        Debug.Log("turn on me");
+    }
+}
---
+++ iHouseSim/Assets/checkPointManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkPointManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Checkpoint KITCHEN;
+    public Checkpoint TV;
+     public static checkPointManager I;
+     void Awake()
+     {
+         I = this;
+     }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum CheckPointV
+{
+
+}
