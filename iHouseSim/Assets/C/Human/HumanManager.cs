db7d6da08b96df5e14a276a6ae9e862b7291910d
Toan Nguyen Mau <nmtoan91@gmail.com>
2019-06-19 19:46:07 +0900

    g

---
+++ iHouseSim/Assets/C/Human/HumanManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanManager : MonoBehaviour
+{
+    public static HumanManager I;
+    void Awake()
+    {
+        I = this;
+    }
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    public void AddHuman(int id)
+    {
+
+    }
+}
---
+++ iHouseSim/Assets/C/Sensor/SensorManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SensorManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //List<Sensor>
+    public static SensorManager I;
+    void Awake()
+    {
+        I = this;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
