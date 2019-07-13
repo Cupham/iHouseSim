16d7786426c824dcf94a60e806c1722ca191ccaa
Cu Pham <cupham@jaist.ac.jp>
2019-07-13 12:10:15 +0900

    add monitoring GW

---
+++ MonGW/run.sh
@@ -0,0 +1,3 @@
+ mvn install:install-file -Dfile=echowand-1.1.jar -DgroupId=jaist.tanlab -DartifactId=echowand -Dpackaging=jar -Dversion=1.1
+
+ java -jar NodeFinder-0.0.1-SNAPSHOT-jar-with-dependencies.jar -i lan0
